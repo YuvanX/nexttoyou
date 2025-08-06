6b95a130b2459e555c8bd8ebabf0d7be08fe9158
YuvanX <abhivigneshofficial@gmail.com>
2025-08-06 12:24:04 +0530

    feat: add notselected enum to usertype

---
+++ db/prisma/migrations/20250805135859_added_not_selected_to_user_type_enum/migration.sql
@@ -0,0 +1,3 @@
+-- AlterEnum
+ALTER TYPE "public"."UserType" ADD VALUE 'NotSelected';
+
---
+++ db/prisma/migrations/20250805140730_set_usertype_default_to_notselected/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."users" ALTER COLUMN "userType" SET DEFAULT 'NotSelected';
