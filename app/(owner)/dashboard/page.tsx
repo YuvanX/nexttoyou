c2195d2c1c00f6a2357a39985238795d258f4dca
YuvanX <abhivigneshofficial@gmail.com>
2025-08-10 11:56:27 +0530

    fix: add datatable to page

--- app/(owner)/dashboard/page.tsx
+++ app/(owner)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { DashBoard } from "@/components/ui/dashboard-section";
+import { DataTable } from "@/components/ui/data-table";
 import { SidebarInset } from "@/components/ui/sidebar";
 import { SiteHeader } from "@/components/ui/site-header";
 import { StatCard } from "@/components/ui/stats-card";
@@ -8,6 +9,7 @@ export default function OwnerDashBoard() {
     return <SidebarInset>
             <SiteHeader />
            <StatCard />
+           <DataTable />
         </SidebarInset>
 
 }
